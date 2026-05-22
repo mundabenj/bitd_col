3ddb4aceb8ec3547032f06fedd42f6f13753f0ff
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2026-05-22 12:57:29 +0300

    start time

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ function greetings(fname) {
     }
     return greeting + " " + fname + "!";
 }
+
+function starTime() {
+           // Get the current date and time
+        var today = new Date();
+        var day = today.getDate();
+
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var month = months[today.getMonth()]; // Months are zero-based
+
+        var year = today.getFullYear();
+
+        var hours = today.getHours();
+        var minutes = today.getMinutes();
+        var seconds = today.getSeconds();
+        document.getElementById("realTime").innerHTML = day + " " + month + " " + year + " " + hours + ":" + minutes + ":" + seconds;
+        t=setTimeout(starTime, 1000); // Refresh the time every second
+}
